acc2e61610506a2be1c9a8d7fa5c7feeda8b3b15
JOYCEmwangi8880 <joyce@student.moringaschoolcom>
2023-09-13 03:20:45 +0000

    completed assignments

---
+++ index.js
@@ -0,0 +1,27 @@
+function makeInt(string){
+    return parseInt(string, 10);
+}
+function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+
+  function add(j, w) {
+    return j + w;
+  }
+  function subtract(j, w) {
+    return j- w;
+  }
+  function multiply(j, w) {
+    return j * w;
+  }
+  function divide(j, w) {
+    return j / w;
+  }
+  function increment(n) {
+    return n + 1;
+  }
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  
